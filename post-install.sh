ecd90de363e17fda9628d02dca8d7ebfb1a921f1
RBFraphael <rbfraphael@gmail.com>
2024-01-23 10:46:41 -0300

    Adicionado Todoist

--- post-install.sh
+++ post-install.sh
@@ -219,6 +219,7 @@ flatpak install flathub org.sqlitebrowser.sqlitebrowser -y
 flatpak install flathub org.telegram.desktop -y
 flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y
+flatpak install flathub com.todoist.Todoist -y
 
 
 
